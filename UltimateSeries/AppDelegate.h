18c1ded4b9809b94b8e77fd4b8ed0e4b2d93f3af
Fernando Vega <fernandovega@ono.com>
2017-07-05 18:04:10 +0200

    Initial Commit

---
+++ UltimateSeries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UltimateSeries
+//
+//  Created by Fernando Vega Giganto on 5/7/17.
+//  Copyright © 2017 Fernando Vega Giganto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UltimateSeries/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UltimateSeries
+//
+//  Created by Fernando Vega Giganto on 5/7/17.
+//  Copyright © 2017 Fernando Vega Giganto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
